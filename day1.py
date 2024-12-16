32f22c96dfe92c8296324429739757e16f9e0751
Husayn <khaybabz@gmail.com>
2024-12-16 23:31:54 +0100

    Learing python with 100 days of python from Udemy

---
+++ day1.py
@@ -0,0 +1,23 @@
+# print("Hello World!\nHello World!\nHello World!\nHello World!")
+# print("Hello" + " " + "Husayn")
+
+# input("What is your name? ")
+
+#This is to input() function to receive a response from users
+# print("Hello " + input(("What is your name? ")) + "!")
+
+
+#Python variables
+# Values can be assigned to a variables: It means storing data into a box.
+name = "Jack"
+print(name)
+
+name = "Husayn"
+print(name)
+print(len(name)) # Method 1: to print the length of the string
+print(len(input(("What is your name? ")))) # Methond 2: to print the length of the string
+
+username = input("What is your name?: ")
+lenght = len(username)
+
+print(lenght)
